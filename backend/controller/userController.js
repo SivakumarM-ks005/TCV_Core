a0b482836ec5955fd7cce494cf1ef27dd6123792
sivakumar-mani <sivakumar.pmani@gmail.com>
2026-02-18 22:06:04 +0530

    forgotpassword
    forgotpassword

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connection = require('../connection');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer')
 
 const login = async (req, res) => {
     const user = req.body;
@@ -38,5 +39,45 @@ const login = async (req, res) => {
     })
 }
 
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth:{
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+});
 
-module.exports = { login }
+const forgotPassword = async(req,res)=>{
+    const user = req.body;
+    query = "SELECT * FROM user WHERE userName=?";
+    connection.query(query, [user.userName], (error, results)=>{
+        try {
+            if(results.length <=0){
+            return res.status(401).json({
+                message: "User Name is not found, please register the email"
+            })
+        }else {
+            var mailOptions ={
+                from: 'timecablevision@gmail.com',
+                to: results[0].email,
+                subject: 'Password Reset',
+                html: '<p>Your login details Email:</p>' + results[0].email + '<p>Passord:</p>' + results[0].password + '<a href="http://local:4200>Click to Login</a>'
+            };
+            transporter.sendMail(mailOptions,(error, info)=>{
+                if(error){
+                    console.log(error)
+                }else {
+                    console.log("Email send successfull", info.response)
+                }
+            });
+            return res.status(200).json({
+                message: "Password sent successfull to your email id"
+            })
+        }
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    })
+}
+
+module.exports = { login, forgotPassword }
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { login } = require('../controller/userController')
+const { login, forgotPassword } = require('../controller/userController')
 
 router.post('/login', login);
+router.post('/forgot-password', forgotPassword);
 
 module.exports = router;
